ea6030ac8b3522a534cc1ecc556b6945c0f12331
Danny Romero <fdromero32@gmail.com>
2020-05-17 08:55:27 -0700

    Used resource generator to add portoflio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-
+  
+ #homepage routes
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   
   # blog
   resources :blogs
+
+  #portfolio
+  resources :portfolios
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
