43370551b64f61eeac5b2b85b57eb06c2c4e3745
Danny Romero <fdromero32@gmail.com>
2020-06-09 18:51:49 -0700

    Implemented Content Tag Helper Methdod & Added View Helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #Devise WhiteList
@@ -14,9 +15,17 @@ class ApplicationController < ActionController::Base
     session[:source] = params[:q] if params[:q]
   end
 
-  #Current User
+  #Current User or Guest
   def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest User",
+                  first_name: "Guest",
+                  last_name: "User",
+                  email: "guest@example.com"
+                  )
   end
 
   #Set Title
